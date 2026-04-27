8f4f984c954a307673a3736d46f64fed5a9689fc
sy <shamilyaz28@gmail.com>
2026-04-27 02:59:46 +0100

    feat(divide-and-conquer): added problems to rotated sorted array

--- patterns/divide-and-conquer/findRotatedIndex.js
+++ patterns/divide-and-conquer/findRotatedIndex.js
@@ -33,3 +33,11 @@ function findRotatedIndex(arr, target) {
 
     return -1;
 }
+
+
+// findRotatedIndex([3,4,1,2],4) // 1
+// findRotatedIndex([6, 7, 8, 9, 1, 2, 3, 4], 8) // 2
+// findRotatedIndex([6, 7, 8, 9, 1, 2, 3, 4], 3) // 6
+// findRotatedIndex([37,44,66,102,10,22],14) // -1
+// findRotatedIndex([6, 7, 8, 9, 1, 2, 3, 4], 12) // -1
+// findRotatedIndex([11,12,13,14,15,16,3,5,7,9], 16) // 5
